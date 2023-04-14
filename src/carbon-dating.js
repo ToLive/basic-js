4b3e84e0e63579d27e6f70add2e26a37dd5bd2f2
Sergey Grabak <seventyseventh@gmail.com>
2023-04-14 23:43:08 +0200

    feat: carbon dating ready

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const sample = Number(sampleActivity);
+
+  if (typeof sampleActivity !== 'string' || isNaN(sample)) {
+    return false;
+  }
+
+  if (sample <= 0 || sample > 15) {
+    return false;
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
