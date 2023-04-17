b1a3ee1d7eb9adbef597af58f84e03cda53e8a59
Sergey Grabak <seventyseventh@gmail.com>
2023-04-17 23:14:50 +0200

    feat: vigenere cipher ready

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machineType = true) {
+    this.machineType = machineType;
+    this.alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 65));
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  formatKey(messageLength, keyLength, key) {
+    key = key.toUpperCase()
+
+    if (messageLength < keyLength) {
+      key = key.slice(0, messageLength);
+    }
+
+    if (messageLength >= keyLength) {
+      const diffLength = Math.ceil(messageLength / keyLength);
+
+      key = key.repeat(diffLength).slice(0, messageLength);
+    }
+
+    return key.split('');
+  }
+
+  formatResult(result) {
+    return (this.machineType)
+      ? result.join('')
+      : result.reverse().join('');
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    key = this.formatKey(message.length, key.length, key);
+
+    const result = message.toUpperCase().split('').reduce((acc, char) => {
+      const charIndex = this.alphabet.indexOf(char);
+
+      return charIndex < 0
+        ? [...acc, char]
+        : [...acc, this.alphabet[(charIndex + this.alphabet.indexOf(key.shift())) % this.alphabet.length]];
+    }, []);
+
+    return this.formatResult(result);
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    key = this.formatKey(message.length, key.length, key);
+
+    const result = message.toUpperCase().split('').reduce((acc, char) => {
+      const charIndex = this.alphabet.indexOf(char);
+
+      return charIndex < 0
+        ? [...acc, char]
+        : [...acc, this.alphabet[(charIndex + this.alphabet.length - this.alphabet.indexOf(key.shift())) % this.alphabet.length]];
+    }, []);
+
+    return this.formatResult(result);
   }
 }
 
